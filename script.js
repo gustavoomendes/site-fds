276bd24f95ef6fc4dd293edfea48884adf7a628a
gustavoomendes <129622211+gustavoomendes@users.noreply.github.com>
2025-05-18 21:27:51 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function mostrarMensagem() {
-    const mensagem = "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna. (João 3:16)";
-    document.getElementById("mensagem").innerText = mensagem;
-}
